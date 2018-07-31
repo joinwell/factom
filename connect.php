3f044b66abb236fb34a06e57a576656ce3c84aa2
ksu <ksu.zhytomirsky@gmail.com>
2018-07-31 11:05:29 +0300

    cron with api

--- connect.php
+++ connect.php
@@ -1,6 +1,14 @@
-<?php
+﻿<?php
 require_once 'libs/funcs.php';
 
+/*
+ * call on restart
+ * docker exec -i -t factom-all /go/bin/factom-walletd
+ * check on restart
+ * docker exec -i factom-all /go/bin/factom-cli -s courtesy-node.factom.com:80 properties
+ *
+ * docker exec -i factom-all /go/bin/factom-cli -s courtesy-node.factom.com:80 importaddress Es49rq6MUgTrmEYNB61eWCpyX1gNLuvfM9mpL4oGDXq53bwX8Rko
+ */
 define('FACTOM_CHAIN_ID', '90162c4247c4ef5eab7b9161a0a5f56c6db437548453db214413c85ea44c4a53');
 define('FACTOM_PUB', 'EC2dnBDuLb9gWLbSLZn8QbAKsox52vq9bCrrZUATqPMttcwVwFdw');
 
@@ -8,6 +16,8 @@ define('FACTOM_NODE', 'courtesy-node.factom.com:80');
 define('FACTOM_WALLET_URL', 'http://10.185.89.1:8089/v2');
 define('FACTOM_API_URL', 'http://courtesy-node.factom.com:80/v2'); //http://10.185.89.1:8088/v2');
 define('FACTOM_API_URL_V1', 'http://courtesy-node.factom.com:80/v1'); //http://10.185.89.1:8088/v1');
+
+define('WELL_API_URL', 'https://well-api.joinwell.com');
 
 //check balance
 $test = curl(FACTOM_API_URL,
@@ -31,22 +41,61 @@ exec($cli_command, $result);
 $all_entries = []; //can cache in db
 foreach ($result AS $row) {
     if (!$row || strpos($row, 'patient_id') === false) continue;
-    $all_entries[] = $row;
+    $row = str_replace('\'', '"', substr($row, 1, strlen($row) - 2));
+    $json = json_decode($row, true);
+    if ($json) {
+        $all_entries[$json['room']] = $json;
+    }
 }
-var_dump($all_entries);
+
 
 //login
-$auth = curl('https://well-api.joinwell.com/api/auth', array(
+$auth = well_post(WELL_API_URL . '/api/auth', array(
     'email' => 'provider@demo.com',
     'password' => 'password',
     'partnerid' => '4'
 ));
-//here will be parse of auth token and send request to data
+$user_token = $auth['token'];
+$user_id = $auth['user']['id'];
+if (!$user_token) {
+    die('need to check api, auth token is empty');
+}
 
 
-exit;
-$json = json_encode('any json data');
-$cli_command = 'echo ' . $json . ' | docker exec -i -t factom-all /go/bin/factom-cli -s ' . FACTOM_NODE . ' addentry -c ' . FACTOM_CHAIN_ID . ' ' . FACTOM_PUB;
-echo $cli_command;
+//get waiting room
+$waiting_room = well_get(WELL_API_URL . '/api/provider/getwaitingroom/1', $user_token);
+if (!$waiting_room) {
+    //create waiting room for tests cases (in general - not needed!)
+    $add_to_waiting_room = well_post(WELL_API_URL . '/api/patient/addtowaitingroom', array(
+        'patient_id' => 5,
+        'provider_id' => 1
+    ), $user_token);
+    if (!@$add_to_waiting_room['status']) {
+        die('need to check api, add to waiting room is not working');
+    }
+}
+
+//get waiting room data
+$waiting_room_data = well_get(WELL_API_URL . '/api/provider/waitingroom/5/1', $user_token);
+if (!$waiting_room_data) {
+    die('need to check api, get waiting room data is not working');
+}
+$file_mark = $waiting_room_data['room'];
+if (isset($all_entries[$waiting_room_data['room']])) {
+    die('room ' . $waiting_room_data['room'] . ' is already saved');
+}
+if (@file_exists($file_mark)) {
+    die('room ' . $waiting_room_data['room'] . ' is already sent');
+}
+$json = json_encode($waiting_room_data);
+
+$cli_command = 'echo \'' . $json . '\' | docker exec -i factom-all /go/bin/factom-cli -s ' . FACTOM_NODE . ' addentry -c ' . FACTOM_CHAIN_ID . ' ' . FACTOM_PUB;
+
+echo '-----------------------------------------------------------------------------------------------------------' . PHP_EOL;
+echo date('Y-m-d H:i:s') . PHP_EOL;
+echo $cli_command . PHP_EOL . PHP_EOL;
 exec($cli_command, $result);
-var_dump($result);
+
+if($result) {
+    file_put_contents($file_mark, 'saved');
+}
--- libs/funcs.php
+++ libs/funcs.php
@@ -1,9 +1,118 @@
 <?php
+date_default_timezone_set('America/Los_Angeles');
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
+function well_get($url, $user_token, $debug = false) {
+    echo '-----------------------------------------------------------------------------------------------------------' . PHP_EOL;
+    echo date('Y-m-d H:i:s') . PHP_EOL;
+    echo $url . PHP_EOL;
+
+    $curl = curl_init();
+
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "GET",
+        CURLOPT_HTTPHEADER => array(
+            "Authorization: Bearer " . $user_token,
+            "Cache-Control: no-cache"
+        ),
+    ));
+
+    $res = curl_exec($curl);
+    if (!$res) {
+        $error = curl_error($curl) . '(' . curl_errno($curl) . ')';
+        die($error);
+    }
+    if (strpos($res, 'Token has expired') > 0) {
+        die('token has expired');
+    }
+    if (strpos($res, '<!DOCTYPE html>') === 0) {
+        die($res);
+    }
+
+    $result = json_decode($res, true);
+    if (@$result['error']) {
+        die('ERROR! ' . $res);
+    }
+    if ($debug) {
+        echo '=>' . PHP_EOL;
+        if (@$result['result']) {
+            echo json_encode($result['result']) . PHP_EOL . PHP_EOL . PHP_EOL;
+        } else {
+            echo $res . PHP_EOL . PHP_EOL . PHP_EOL;
+        }
+    }
+
+    return @$result;
+}
+
+function well_post($url, $fields, $user_token = false, $debug = false)
+{
+    echo '-----------------------------------------------------------------------------------------------------------' . PHP_EOL;
+    echo date('Y-m-d H:i:s') . PHP_EOL;
+    echo $url . PHP_EOL;
+
+    $curl = curl_init();
+
+    $fields_string = array();
+    foreach ($fields AS $key => $val) {
+        $fields_string[] = "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"$key\"\r\n\r\n$val\r\n";
+    }
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    $headers = array(
+        "Cache-Control: no-cache",
+        "Postman-Token: 24f288c8-4a9e-4720-b607-085730e60a97",
+        "content-type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW"
+    );
+    if ($user_token) {
+        $headers[] = "Authorization: Bearer " . $user_token;
+    }
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "POST",
+        CURLOPT_POSTFIELDS => implode('',$fields_string) . "------WebKitFormBoundary7MA4YWxkTrZu0gW--",
+        CURLOPT_HTTPHEADER => $headers
+    ));
+
+    $res = curl_exec($curl);
+    if (!$res) {
+        $error = curl_error($curl) . '(' . curl_errno($curl) . ')';
+        die($error);
+    }
+    if (strpos($res, 'Token has expired') > 0) {
+        die('token has expired');
+    }
+    if (strpos($res, '<!DOCTYPE html>') === 0) {
+        die($res);
+    }
+
+    $result = json_decode($res, true);
+    if (@$result['error']) {
+        die('ERROR! ' . $res);
+    }
+    if ($debug) {
+        echo '=>' . PHP_EOL;
+        if (@$result['result']) {
+            echo json_encode($result['result']) . PHP_EOL . PHP_EOL . PHP_EOL;
+        } else {
+            echo $res . PHP_EOL . PHP_EOL . PHP_EOL;
+        }
+    }
+
+    return @$result;
+}
+
 function curl($url, $fields, $debug = false)
 {
+    echo '-----------------------------------------------------------------------------------------------------------' . PHP_EOL;
+    echo date('Y-m-d H:i:s') . PHP_EOL;
+
     $curl = curl_init();
     curl_setopt($curl, CURLOPT_FOLLOWLOCATION, TRUE);
     curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
@@ -18,25 +127,22 @@ function curl($url, $fields, $debug = false)
         die($error);
     }
     $json = @json_decode($res, true);
-    if (@$json['code'] && @$json['error']) {
+    if (@$json['code'] || @$json['error']) {
         echo PHP_EOL . $url . PHP_EOL;
         var_dump($json);
         die('error!');
     }
 
-
-    echo '-----------------------------------------------------------------------------------------------------------' . PHP_EOL;
-    echo date('Y-m-d H:i:s') . PHP_EOL;
     $fields_2 = $fields;
     if (isset($fields_2['id'])) unset($fields_2['id']);
     if (isset($fields_2['jsonrpc'])) unset($fields_2['jsonrpc']);
     echo json_encode($fields_2) . PHP_EOL;
-    echo '=>' . PHP_EOL;
     $result = json_decode($res, true);
     if (@$result['error']) {
         die('ERROR! ' . $res);
     }
     if ($debug) {
+        echo '=>' . PHP_EOL;
         if (@$result['result']) {
             echo json_encode($result['result']) . PHP_EOL . PHP_EOL . PHP_EOL;
         } else {
